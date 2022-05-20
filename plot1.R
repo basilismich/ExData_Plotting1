23dd9704627385cd1590ff0c301a5917d57ee4a1
Vasilis Michailidis <100042190+basilismich@users.noreply.github.com>
2022-05-20 16:13:24 +0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,1 +1,16 @@
+library("data.table")
+data<-"household_power_consumption.txt"
+electricDT<- fread(data,na.strings="?")
+# Change Date Column to Date Type
+electricDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
 
+# we want from 2007-02-01 to 2007-02-02 , so we filter the dates
+electricDT <- electricDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+
+png("plot2.png", width=480, height=480)
+
+##Finally  Plot 1
+hist(electricDT[, Global_active_power], main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+dev.off()
