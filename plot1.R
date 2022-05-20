97d83d052babd51877d53659b4d9e42bd22312ff
Vasilis Michailidis <100042190+basilismich@users.noreply.github.com>
2022-05-20 16:13:39 +0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -7,7 +7,7 @@ electricDT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
 # we want from 2007-02-01 to 2007-02-02 , so we filter the dates
 electricDT <- electricDT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
 
-png("plot2.png", width=480, height=480)
+png("plot1.png", width=480, height=480)
 
 ##Finally  Plot 1
 hist(electricDT[, Global_active_power], main="Global Active Power", 
